5447b21b7640dfecb70bbd8574a7e0811ffc8568
eopicla <102342301+eopicla@users.noreply.github.com>
2024-09-04 18:49:14 -0500

    simple ingot-nugget recipes added

--- src/main/java/com/ultricem/exoterra/datagen/ModRecipeProvider.java
+++ src/main/java/com/ultricem/exoterra/datagen/ModRecipeProvider.java
@@ -1,6 +1,7 @@
 package com.ultricem.exoterra.datagen;
 
 import com.ultricem.exoterra.ExoTerra;
+import com.ultricem.exoterra.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
@@ -19,6 +20,19 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(RecipeOutput pRecipeOutput) {
+
+        // NOTE: Ingot > nugget and back again recipes in block below.
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.IRIDIUM_NUGGET.get(), 9).requires(ModItems.IRIDIUM_INGOT.get()).unlockedBy("has_iridium_ingot", has(ModItems.IRIDIUM_INGOT.get())).save(pRecipeOutput);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.ALUMINUM_NUGGET.get(), 9).requires(ModItems.ALUMINUM_INGOT.get()).unlockedBy("has_aluminum_ingot", has(ModItems.ALUMINUM_INGOT.get())).save(pRecipeOutput);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.TIN_NUGGET.get(), 9).requires(ModItems.TIN_INGOT.get()).unlockedBy("has_tin_ingot", has(ModItems.TIN_INGOT.get())).save(pRecipeOutput);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.TITANIUM_NUGGET.get(), 9).requires(ModItems.TITANIUM_INGOT.get()).unlockedBy("has_titanium_ingot", has(ModItems.TITANIUM_INGOT.get())).save(pRecipeOutput);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.STEEL_NUGGET.get(), 9).requires(ModItems.STEEL_INGOT.get()).unlockedBy("has_steel_ingot", has(ModItems.STEEL_INGOT.get())).save(pRecipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.IRIDIUM_INGOT.get()).pattern("NNN").pattern("NNN").pattern("NNN").define('N', ModItems.IRIDIUM_NUGGET.get()).unlockedBy("has_iridium_nugget", has(ModItems.IRIDIUM_NUGGET.get())).save(pRecipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.ALUMINUM_INGOT.get()).pattern("NNN").pattern("NNN").pattern("NNN").define('N', ModItems.ALUMINUM_NUGGET.get()).unlockedBy("has_aluminum_nugget", has(ModItems.ALUMINUM_NUGGET.get())).save(pRecipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.TIN_INGOT.get()).pattern("NNN").pattern("NNN").pattern("NNN").define('N', ModItems.TIN_NUGGET.get()).unlockedBy("has_tin_nugget", has(ModItems.TIN_NUGGET.get())).save(pRecipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.TITANIUM_INGOT.get()).pattern("NNN").pattern("NNN").pattern("NNN").define('N', ModItems.TITANIUM_NUGGET.get()).unlockedBy("has_titanium_nugget", has(ModItems.TITANIUM_NUGGET.get())).save(pRecipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.STEEL_INGOT.get()).pattern("NNN").pattern("NNN").pattern("NNN").define('N', ModItems.STEEL_NUGGET.get()).unlockedBy("has_steel_nugget", has(ModItems.STEEL_NUGGET.get())).save(pRecipeOutput);
+        // NOTE: Block end
 
     }
 
