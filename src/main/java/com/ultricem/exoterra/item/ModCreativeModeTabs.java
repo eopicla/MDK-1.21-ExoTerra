a190f480f918dfa1864c9b46aafbe042261c7039
eopicla <102342301+eopicla@users.noreply.github.com>
2024-09-04 18:24:31 -0500

    lang + future creativetab buildout

--- src/main/java/com/ultricem/exoterra/item/ModCreativeModeTabs.java
+++ src/main/java/com/ultricem/exoterra/item/ModCreativeModeTabs.java
@@ -7,6 +7,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -21,34 +22,22 @@ public class ModCreativeModeTabs {
                     .title(Component.translatable("creativetab.exoterra.exoterra_items"))
                     .displayItems((itemDisplayParameters, output) -> {
 
-                        output.accept(ModItems.ALUMINA);
                         output.accept(ModItems.ALUMINUM_INGOT);
                         output.accept(ModItems.ALUMINUM_NUGGET);
                         output.accept(ModItems.ALUMINUM_PLATE);
                         output.accept(ModItems.ALUMINUM_POWDER);
                         output.accept(ModItems.ALUMINUM_ROD);
-                        output.accept(ModItems.BAUXITE_ORE_ITEM);
-                        output.accept(ModItems.CASSERITE_ORE_ITEM);
-                        output.accept(ModItems.COMMON_SILICA_SAND);
                         output.accept(ModItems.IRIDIUM_INGOT);
                         output.accept(ModItems.IRIDIUM_NUGGET);
-                        output.accept(ModItems.IRIDIUM_ORE_ITEM);
                         output.accept(ModItems.IRIDIUM_PLATE);
                         output.accept(ModItems.IRIDIUM_POWDER);
                         output.accept(ModItems.IRIDIUM_ROD);
-                        output.accept(ModItems.MAGNESITE_ORE_ITEM);
                         output.accept(ModItems.MAGNESIUM_POWDER);
-                        output.accept(ModItems.RAW_ALUMINUM);
-                        output.accept(ModItems.RAW_TITANIUM);
-                        output.accept(ModItems.RUTILE_ORE_ITEM);
-                        output.accept(ModItems.SILICON_BOULE);
-                        output.accept(ModItems.SILICON_WAFER);
                         output.accept(ModItems.STEEL_INGOT);
                         output.accept(ModItems.STEEL_NUGGET);
                         output.accept(ModItems.STEEL_PLATE);
                         output.accept(ModItems.STEEL_POWDER);
                         output.accept(ModItems.STEEL_ROD);
-                        output.accept(ModItems.SULFUR_POWDER);
                         output.accept(ModItems.TIN_INGOT);
                         output.accept(ModItems.TIN_NUGGET);
                         output.accept(ModItems.TIN_PLATE);
@@ -59,13 +48,31 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.TITANIUM_PLATE);
                         output.accept(ModItems.TITANIUM_POWDER);
                         output.accept(ModItems.TITANIUM_ROD);
-                        output.accept(ModItems.ALUMINA);
+
                     }).build());
 
+    public static final Supplier<CreativeModeTab> EXOTERRA_RESOURCES_TAB = CREATIVE_MODE_TAB.register("exoterra_resources_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RAW_TITANIUM.get()))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ExoTerra.MOD_ID, "exoterra_items_tab"))
+                    .title(Component.translatable("creativetab.exoterra.exoterra_resources"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(ModItems.IRIDIUM_ORE_ITEM);
+                        output.accept(ModItems.BAUXITE_ORE_ITEM);
+                        output.accept(ModItems.CASSERITE_ORE_ITEM);
+                        output.accept(ModItems.RUTILE_ORE_ITEM);
+                        output.accept(ModItems.MAGNESITE_ORE_ITEM);
+                        output.accept(ModItems.RAW_ALUMINUM);
+                        output.accept(ModItems.RAW_TITANIUM);
+                        output.accept(ModItems.ALUMINA);
+                        output.accept(ModItems.SULFUR_POWDER);
+                        output.accept(ModItems.COMMON_SILICA_SAND);
+
+                    }).build());
 
     public static final Supplier<CreativeModeTab> EXOTERRA_BLOCKS_TAB = CREATIVE_MODE_TAB.register("exoterra_blocks_tab",
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.BAUXITE_ORE.get()))
-                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ExoTerra.MOD_ID, "exoterra_items_tab"))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ExoTerra.MOD_ID, "exoterra_resources_tab"))
                     .title(Component.translatable("creativetab.exoterra.exoterra_blocks"))
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModBlocks.IRIDIUM_ORE);
@@ -82,6 +89,28 @@ public class ModCreativeModeTabs {
 
                         output.accept(ModBlocks.MAGNESITE_ORE);
                         output.accept(ModBlocks.DEEPSLATE_MAGNESITE_ORE);
+
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> EXOTERRA_COMPONENTS_TAB = CREATIVE_MODE_TAB.register("exoterra_components_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.ARMOR_STAND))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ExoTerra.MOD_ID, "exoterra_blocks_tab"))
+                    .title(Component.translatable("creativetab.exoterra.exoterra_components"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(ModItems.SILICON_BOULE);
+                        output.accept(ModItems.SILICON_WAFER);
+
+                    }).build());
+
+    public static final Supplier<CreativeModeTab> EXOTERRA_MACHINES_TAB = CREATIVE_MODE_TAB.register("exoterra_machines_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.STONECUTTER))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(ExoTerra.MOD_ID, "exoterra_components_tab"))
+                    .title(Component.translatable("creativetab.exoterra.exoterra_machines"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(ModItems.ALUMINA);
+                        // Add contents here as before
 
                     }).build());
 
